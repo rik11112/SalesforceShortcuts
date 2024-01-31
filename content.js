1f63a2cf85981e2ee9b3f921bcffb9f17b7d718b
Rik Puts <rikputs@hotmail.com>
2024-01-31 10:40:20 +0100

    changed shortcut key to ctrl+k

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 document.addEventListener('keydown', function (event) {
-    if (event.ctrlKey && event.key === '/') {
+    if (event.ctrlKey && event.key === 'k') {
         const quickFindInputs = document.querySelectorAll('input[placeholder="Quick Find"]');
 
         // Iterate through all matching elements
